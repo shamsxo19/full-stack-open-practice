134035951e222eebe32bde49ebea0acc65dd12a0
shamsxo19 <shamsyusufkhan940@gmail.com>
2026-08-22 18:24:14 +0530

    Exercises 4.1 and 4.2 done

---
+++ part4/bloglist/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
