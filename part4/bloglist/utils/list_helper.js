a0c0e0ebbf447e93cc1d63d0ae3f20f4878a8d65
shamsxo19 <shamsyusufkhan940@gmail.com>
2026-08-24 18:38:26 +0530

    Exercise 4.3, 4.4 and 4.5 done

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,20 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite
+  })
+}
+
+module.exports = {
+  dummy,
+  totalLikes, favoriteBlog
+}
