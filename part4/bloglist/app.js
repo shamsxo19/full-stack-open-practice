b2642ec9fd50d48497a22a2d565ead52950a9094
shamsxo19 <shamsyusufkhan940@gmail.com>
2026-09-22 16:24:39 +0530

    Exercise 4.18 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blog')
-const usersRouter = require('./controllers/users')  
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')  
 const middleware = require('./utils/middleware')
 const app = express()
 
@@ -23,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
